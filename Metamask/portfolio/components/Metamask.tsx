10857f6e1902d122c792a9969ce567460b7623f8
Juraj Bačovčin <juraj.bacovcin@tatum.io>
2023-06-16 17:10:34 +0200

    ALL-1647 Add getting and processing of balances

--- Metamask/portfolio/components/Metamask.tsx
+++ Metamask/portfolio/components/Metamask.tsx
@@ -9,10 +9,12 @@ import { toast } from "react-hot-toast";
 import Button from "./Button";
 import Card from "./Card";
 import Loading from "./Loading";
+import { processBalances } from "@/lib/utils";
 
 const Metamask = (): JSX.Element => {
   const [loading, setLoading] = React.useState(false);
   const [account, setAccount] = React.useState("");
+  const [balances, setBalances] = React.useState([{}]);
 
   const connectMetamask = async () => {
     setLoading(true);
@@ -25,9 +27,18 @@ const Metamask = (): JSX.Element => {
       /* https://docs.tatum.com/docs/wallet-provider/metamask/connect-a-wallet */
       const acc = await tatum.walletProvider.metaMask.connect();
 
-      // TODO
+      /* https://docs.tatum.com/docs/wallet-address-operations/get-all-assets-the-wallet-holds */
+      const bal = await tatum.address.getBalance({
+        addresses: [
+          "0x4edd6b97add54cab38f767618d19d04992a3d55a" /* TODO: replace with acc */,
+        ],
+      });
+
+      // TODO: Discuss whether there is a faster way
+      console.log(await processBalances(bal.data, tatum.nft));
 
       setAccount(acc);
+      setBalances(bal.data);
     } catch (error) {
       console.error(error);
       toast.error("Connection failed");
--- Metamask/portfolio/lib/utils.ts
+++ Metamask/portfolio/lib/utils.ts
@@ -1,11 +1,79 @@
-import { AddressBalance } from "@tatumcom/js";
+import { AddressBalance, Nft } from "@tatumcom/js";
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+interface Token {
+  label: string;
+  address: string;
+}
+
+interface NftToken extends Token {
+  id: string;
+  metadata?: Record<string, unknown>;
+}
+
+export interface Balances {
+  coin: string;
+  erc20: Token[];
+  erc721: NftToken[];
+  erc1155: NftToken[];
+}
+
+const getTokenLabel = (bal: string, asset?: string) =>
+  asset ? `${bal} ${asset}` : bal;
+
+const getTokenAddress = (address?: string) => address || "?";
+
+const getTokenId = (id?: string) => id || "";
+
+/* https://docs.tatum.com/docs/nfts */
+const getMetadata = async (nft: Nft, address?: string, id?: string) => {
+  if (!address || !id) return {};
+  const nftDetail = await nft.getNftMetadata({
+    tokenAddress: address,
+    tokenId: id,
+  });
+  return nftDetail.data && nftDetail.data.metadata
+    ? nftDetail.data.metadata
+    : {};
+};
 
 /* Merge classes with tailwind-merge with clsx full feature */
 export const clsxm = (...classes: ClassValue[]) => twMerge(clsx(...classes));
 
 /* Process wallet balances in the desired format */
-export const processBalances = (data: AddressBalance[]) => {
-  // TODO
+export const processBalances = async (data: AddressBalance[], nft: Nft) => {
+  const balances: Balances = {
+    coin: "",
+    erc20: [],
+    erc721: [],
+    erc1155: [],
+  };
+
+  for (const bal of data) {
+    if (bal.type === "native") {
+      balances.coin = `${bal.balance} ${bal.asset}`;
+    } else if (bal.type === "fungible") {
+      balances.erc20.push({
+        label: getTokenLabel(bal.balance, bal.asset),
+        address: getTokenAddress(bal.tokenAddress),
+      });
+    } else if (bal.type === "nft") {
+      balances.erc721.push({
+        label: getTokenLabel(bal.balance, bal.asset),
+        address: getTokenAddress(bal.tokenAddress),
+        id: getTokenId(bal.tokenId),
+        metadata: await getMetadata(nft, bal.tokenAddress, bal.tokenId),
+      });
+    } else if (bal.type === "mutlitoken") {
+      balances.erc1155.push({
+        label: getTokenLabel(bal.balance, bal.asset),
+        address: getTokenAddress(bal.tokenAddress),
+        id: getTokenId(bal.tokenId),
+        metadata: await getMetadata(nft, bal.tokenAddress, bal.tokenId),
+      });
+    }
+  }
+
+  return balances;
 };
